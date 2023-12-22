6504fbb64efa20454d3292a636081d961e028285
Paul Wells <wellsy95@hotmail.com>
2023-12-22 17:19:13 +0000

    search functionality finished

--- menu.py
+++ menu.py
@@ -5,10 +5,22 @@ Enter one of the following:
 - 'b' to look at 5 star books
 - 'c' to look at the cheapest books
 - 'n' to get the next available book
+- 's' to search by book title
 - 'q' to quit
 enter your choice: '''
 
 original_books = books.copy() # need a copy, otherwise the sorting functions will sort the same list permanently
+
+def search_books_by_keyword():
+    keyword = input("Enter a keyword to search in book titles: ")
+    matching_books = [book for book in original_books if keyword.lower() in book.name.lower()]
+
+    if matching_books:
+        for book in matching_books:
+            print(book)
+    else:
+        print(f"No books found with the keyword '{keyword}'.")
+
 
 def print_best_books():
     best_books = sorted(original_books, key=lambda x: x.rating, reverse=True)[:10] # sorts by the x.rating property, first 10 indexes
@@ -45,6 +57,9 @@ def menu():
 
             elif user_input == "n":
                 get_next_book()
+
+            elif user_input == "s":
+                search_books_by_keyword()
 
             else:
                 print("invalid option")
