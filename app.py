9aa9d4d3dcc74df9cc2d6def395f3b00b90205ad
Paul Wells <wellsy95@hotmail.com>
2023-12-26 11:27:36 +0000

    async working

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import requests
 from itertools import chain
 import logging
 from pages.book_page import BooksPage
+import aiohttp
+import asyncio
+import time
 
 logging.basicConfig(
     format='%s:%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
@@ -20,11 +23,36 @@ page = BooksPage(page_content)
 
 books = page.books
 
-for page_num in range(1, page.page_count):
-    url = f'http://books.toscrape.com/catalogue/page-{page_num + 1}.html'
-    page_content = requests.get(url).content
-    logger.debug("Creating BooksPage from page_content ...")
-    page = BooksPage(page_content)
-    books = list(chain(books, page.books))  # Convert the chain object to a list, so it can be copied in menu.py
+# asynchronous requests
 
-# Now 'books' is a list containing all items from the original 'books' and the new 'page.books'
+loop = asyncio.get_event_loop()
+
+async def fetch_page(session, url):
+    page_state = time.time()
+    async with asyncio.timeout(10):
+        async with session.get(url) as response:
+                print(f'page took {time.time() - page_state}')
+                return await response.text()
+    # each fetch_page has a timeout(10 seconds) and awaits the response, so others can run concurrently, and returns tha response text aka html content
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+
+        return await grouped_tasks # returned as individual elements in a list of results of all the tasks that were ran, i.e. a list of html content from each page
+    # above, opens a session, and for each url runs the fetch_page function (concurrently) and then .gather awaits all tasks to be completed before proceeding
+
+urls = [f'http://books.toscrape.com/catalogue/page-{page_num + 1}.html' for page_num in range(1, page.page_count)]
+
+start = time.time()
+
+pages = loop.run_until_complete(get_multiple_pages(loop, *urls)) # i.e. runs the fetch_page for each url, using the event loop
+
+print(f'took {time.time() - start}')
+
+for page_content in pages:
+    page = BooksPage(page_content)
+    books = list(chain(books, page.books)) # adds the BookPage content from each page to the books list
